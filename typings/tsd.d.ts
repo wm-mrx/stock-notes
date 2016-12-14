776aee05279b1ebbe7bdb507ac28162dfbb2b3ab
Vai Wilmintje <Vai Wilmintje>
2016-12-14 18:40:19 +0700

    modified typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -14,5 +14,4 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="sequelize/sequelize.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
-/// <reference path="socket.io/socket.io.d.ts" />
 /// <reference path="validator/validator.d.ts" />
